033c18bf845c4739a92e0f6fda8305e10c849977
Max Gabriel Susman <gabe.susman@gmail.com>
2025-05-29 02:11:53 +0000

    fixed linting errors in launch/rsp.launch.py

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -12,24 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
-
-from ament_index_python.packages import get_package_share_directory
-
 from launch import LaunchDescription
 from launch.substitutions import LaunchConfiguration
 from launch.actions import DeclareLaunchArgument
-from launch_ros.actions import Node
-
-import xacro
 
 
 def generate_launch_description():
-
-    # Check if we're told to use sim time
-    use_sim_time = LaunchConfiguration('use_sim_time')
-
-    # Launch!
     return LaunchDescription([
         DeclareLaunchArgument(
             'use_sim_time',
